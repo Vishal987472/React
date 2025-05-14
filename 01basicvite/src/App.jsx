3506aa65becf21531e0f8c31cf6e554686a7587c
vishal <pal987472@gmail.com>
2025-05-14 22:30:09 +0530

    upload

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,12 @@
+import First from "./first";
+
+function App() {
+  return (
+    <>
+      <First />
+      <h1>Hello Vite + React!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/first.jsx
@@ -0,0 +1,10 @@
+function First() {
+  return (
+    <div>
+      <h1>First Component</h1>
+      <p>This is the first component in our Vite app.</p>
+    </div>
+  );
+}
+
+export default First;
